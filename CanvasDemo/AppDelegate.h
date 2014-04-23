74324a0507ef7255e3b2677d22c54cb7cc8e4d68
apple <taagoomacmini@apple.local>
2014-04-23 14:12:34 +0800

    Initial Commit

---
+++ CanvasDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CanvasDemo
+//
+//  Created by apple on 14-4-23.
+//  Copyright (c) 2014年 Taagoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CanvasDemoTests/CanvasDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  CanvasDemoTests.m
+//  CanvasDemoTests
+//
+//  Created by apple on 14-4-23.
+//  Copyright (c) 2014年 Taagoo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CanvasDemoTests : XCTestCase
+
+@end
+
+@implementation CanvasDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
